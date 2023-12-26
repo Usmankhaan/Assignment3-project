fb8609d30db87798e1b4ced27c090beecba8a669
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 21:11:30 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def helo_world():
     return 'Hello, everyone!'
-def hello_world():
+def heo_world():
     return ' My name is usman'
-def hello_world():
+def ho_world():
     return 'I am stydent of BSCS'
 
 if __name__ == '__main__':
