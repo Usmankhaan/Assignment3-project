8187177ab32bb91f9f1bd7afd3d35decc6bb9cf9
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 21:32:51 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def helo_world():
-    return 'Hello, everyone!\nMy name is Muhammad Usman\nI am student of BSCS'
+    return 'Hello, everyone!<br>My name is Muhammad Usman<br>I am student of BSCS'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
