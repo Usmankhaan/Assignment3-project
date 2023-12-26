422810c373e4c13422bda4830d61aa71e9892e05
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 21:35:59 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,8 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def helo_world():
-    return 'Hello, everyone!<br>My name is Muhammad Usman<br>I am student of BSCS'
+def hello_world():
+    return '''
+        <p>Hello, everyone!</p>
+        <p>My name is Muhammad Usman</p>
+        <p>fa19-bcs-124</p>
+        <button onclick="alert('name!')">Button 1</button>
+        <button onclick="alert('id')">Button 2</button>
+        <button onclick="alert('password')">Button 3</button>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
