32163f95865d15f1cefdc17c7877c2402be5d817
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 14:38:48 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, everyone! my name is usman'
 
-if __name__ == '__main__':
+f __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
