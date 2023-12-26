72797d993f1c645723dc97f3adddb8fc36314d36
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 21:57:00 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,127 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Placeholder for user data (replace with a database in a real application)
+users = {
+    'user1': {'name': 'John Doe', 'registration_number': '12345'},
+    'user2': {'name': 'Jane Doe', 'registration_number': '67890'}
+}
+
 @app.route('/')
 def login_form():
-    return '''
-        <html>
-        <head>
-            <style>
-                body {
-                    background-color: #f5f5f5;
-                    font-family: 'Arial', sans-serif;
-                    margin: 0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                }
+    return render_template('login_form.html')
 
-                .container {
-                    width: 400px;
-                    padding: 30px;
-                    background-color: #fff;
-                    border-radius: 10px;
-                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-                }
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    registration_number = request.form['registration-number']
 
-                h2 {
-                    text-align: center;
-                    color: #333;
-                }
+    # Check if the provided credentials match any user in the database
+    for user, data in users.items():
+        if data['name'] == username and data['registration_number'] == registration_number:
+            # Redirect to the welcome page with the user's name
+            return redirect(url_for('welcome', username=username))
 
-                label {
-                    display: block;
-                    margin: 15px 0 5px;
-                    color: #555;
-                }
+    # If no match, redirect back to the login form
+    return redirect(url_for('login_form'))
 
-                input[type="text"], input[type="password"] {
-                    width: 100%;
-                    padding: 12px;
-                    margin: 8px 0;
-                    display: inline-block;
-                    border: 1px solid #ccc;
-                    box-sizing: border-box;
-                    border-radius: 5px;
-                }
-
-                input[type="submit"] {
-                    background-color: #007bff;
-                    color: white;
-                    padding: 12px 20px;
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                    font-size: 16px;
-                    width: 100%;
-                }
-
-                input[type="submit"]:hover {
-                    background-color: #0056b3;
-                }
-
-                .form-group {
-                    margin-bottom: 20px;
-                }
-
-                .form-group::after {
-                    content: "";
-                    display: table;
-                    clear: both;
-                }
-
-                .form-group label {
-                    float: left;
-                    width: 30%;
-                }
-
-                .form-group input {
-                    float: left;
-                    width: 70%;
-                }
-
-                .form-group:last-child {
-                    margin-bottom: 0;
-                }
-
-                .link {
-                    text-align: center;
-                    margin-top: 20px;
-                }
-
-                .link a {
-                    color: #007bff;
-                    text-decoration: none;
-                }
-
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <h2>Login Form</h2>
-                <form>
-                    <div class="form-group">
-                        <label for="username">Name:</label>
-                        <input type="text" id="username" name="username" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="registration-number">Registration Number:</label>
-                        <input type="text" id="registration-number" name="registration-number" required>
-                    </div>
-
-                    <input type="submit" value="Login">
-                </form>
-
-                <div class="link">
-                    Don't have an account? <a href="#">Sign Up</a>
-                </div>
-            </div>
-        </body>
-        </html>
-    '''
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
