f79f9975bc0e85e4d5c170ae5887ee9f429ec5c9
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 14:48:51 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import tkinter as tk
+from tkinter import messagebox
 
-app = Flask(__name__)
+def login():
+    username = entry_username.get()
+    password = entry_password.get()
 
-@app.route('/')
-def hello_world():
-    return 'Hello, everyone! my name is usman'
+    # Add your authentication logic here
+    # For simplicity, let's use a hardcoded username and password
+    if username == "user" and password == "pass":
+        messagebox.showinfo("Login Successful", "Welcome, {}".format(username))
+    else:
+        messagebox.showerror("Login Failed", "Invalid username or password")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# Create the main window
+root = tk.Tk()
+root.title("Login Form")
+
+# Create and place widgets (labels, entry fields, buttons) in the window
+label_username = tk.Label(root, text="Username:")
+label_username.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+entry_username = tk.Entry(root)
+entry_username.grid(row=0, column=1, padx=10, pady=10)
+
+label_password = tk.Label(root, text="Password:")
+label_password.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+entry_password = tk.Entry(root, show="*")  # Use show="*" to hide the password
+entry_password.grid(row=1, column=1, padx=10, pady=10)
+
+button_login = tk.Button(root, text="Login", command=login)
+button_login.grid(row=2, column=0, columnspan=2, pady=10)
+
+# Start the Tkinter event loop
+root.mainloop()
