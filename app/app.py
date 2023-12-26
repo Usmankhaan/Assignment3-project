9acac9f838c56281e91531027f5d1549f0bbfd60
Muhammad Usman <114286687+Usmankhaan@users.noreply.github.com>
2023-12-26 21:42:20 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,39 +9,52 @@ def hello_world():
         <head>
             <style>
                 body {
-                    background-color: #f4f4f4;
-                    font-family: Arial, sans-serif;
+                    background-color: #f0f0f0;
+                    font-family: 'Arial', sans-serif;
+                    margin: 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
                 }
 
                 .container {
-                    width: 50%;
-                    margin: auto;
-                    overflow: hidden;
-                    padding: 20px;
+                    width: 400px;
+                    padding: 30px;
                     background-color: #fff;
                     border-radius: 10px;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                    margin-top: 50px;
+                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+                }
+
+                h2 {
+                    text-align: center;
+                    color: #333;
+                }
+
+                label {
+                    display: block;
+                    margin: 15px 0 5px;
+                    color: #555;
                 }
 
                 input[type="text"], input[type="password"] {
                     width: 100%;
-                    padding: 8px 12px;
+                    padding: 10px;
                     margin: 8px 0;
                     display: inline-block;
                     border: 1px solid #ccc;
                     box-sizing: border-box;
-                    border-radius: 4px;
+                    border-radius: 5px;
                 }
 
                 input[type="submit"] {
                     background-color: #4caf50;
                     color: white;
-                    padding: 10px 15px;
-                    margin: 8px 0;
+                    padding: 12px 20px;
                     border: none;
-                    border-radius: 4px;
+                    border-radius: 5px;
                     cursor: pointer;
+                    font-size: 16px;
                 }
 
                 input[type="submit"]:hover {
